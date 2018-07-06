43aaade75f13dde1514961aa8232dec4bd1719c3
alexey-lipodat <alexey.lipodat@teamaol.com>
2018-07-06 17:54:05 +0300

    ODP-4821 use deploy section instead of success

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "pushing bandar-log image to dockerhub..."
-  docker login -u=$DOCKERHUB_UNAME -p=$DOCKERHUB_PASS
-  sbt bandarlog/docker:publish
-fi
+echo "pushing bandar-log image to dockerhub..."
+docker login -u=$DOCKERHUB_UNAME -p=$DOCKERHUB_PASS
+sbt bandarlog/docker:publish

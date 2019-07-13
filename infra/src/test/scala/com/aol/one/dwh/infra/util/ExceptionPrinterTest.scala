94db8b10a76d86cb832a3d229882f12130bba789
Gemelen <git@gemelen.net>
2019-07-13 13:21:20 +0300

    Make test to test what it should test

--- infra/src/test/scala/com/aol/one/dwh/infra/util/ExceptionPrinterTest.scala
+++ infra/src/test/scala/com/aol/one/dwh/infra/util/ExceptionPrinterTest.scala
@@ -5,12 +5,18 @@ import org.scalatest.FunSuite
 class ExceptionPrinterTest extends FunSuite {
 
   test("getting stack trace as string") {
-    val exceptionMessage = "My test exception"
-    val e = new Exception(exceptionMessage)
-    object ExceptionPrinterImpl extends ExceptionPrinter {}
-    val stackTraceString = new ExceptionPrinterImpl.ExceptionOpts(e).getStringStackTrace
+    val e = new Exception("sf") with ExceptionPrinter
+    val st = List(
+      new StackTraceElement("f.q.d.n.Class", "c", "Class.java", 30),
+      new StackTraceElement("f.q.d.n.Class", "b", "Class.java", 20),
+      new StackTraceElement("f.q.d.n.Class", "a", "Class.java", 10)
+    )
+    e.setStackTrace(st.toArray)
 
-    assert(stackTraceString.contains(exceptionMessage), "Exception print stack should contain message")
+    val expected: String = st.map(_.toString()).mkString("\n") ++ "\n"
+    val actual: String = e.getStackTraceString
+
+    assert(expected == actual, "Stacktrace converted to string correctly")
   }
 
 }
